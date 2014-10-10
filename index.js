5757533753b35b0a9284606742c97da668d1c510
Cristian Douce <cristian@gravityonmars.com>
2014-10-10 18:34:43 -0300

    Update template.jade path for jade compilation

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 var read = fs.readFileSync;
 var jade = require('jade');
 
-var header = jade.compile(read('template.jade', { encode: 'utf8'}));
+var header = jade.compile(read('./template.jade', { encode: 'utf8'}));
 
 module.exports = render;
 
