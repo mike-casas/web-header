adb92a2d94447e92392864a7bb10b3542567742a
Ramiro Silveyra d'Avila <ramasilveyra1@gmail.com>
2016-06-07 23:47:19 -0300

    Add carte-blanche support for an isolated development space

--- tools/webpack.playground.config.js
+++ tools/webpack.playground.config.js
@@ -3,6 +3,7 @@ import commonConfig from './webpack.common.config.js';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import webpack from 'webpack';
 import path from 'path';
+import CarteBlanche from 'carte-blanche';
 
 const DEBUG = process.env.NODE_ENV !== 'production';
 
@@ -35,6 +36,13 @@ config.plugins.push(
     title: 'Auth0 Web Header Component',
     template: './tools/playground-template.ejs',
     inject: 'body'
+  })
+);
+
+config.plugins.push(
+  new CarteBlanche({
+    componentRoot: './src',
+    dest: 'variations'
   })
 );
 
