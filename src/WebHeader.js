593225f8913e2e9611579a5b9f666256d3b8807a
Ariel Gerstein <ariel.ger@hotmail.com>
2016-06-23 12:53:04 -0300

    Add style guide styles, remove css modules global styles.

--- src/WebHeader.js
+++ src/WebHeader.js
@@ -64,35 +64,48 @@ class WebHeader extends Component {
 
   render() {
     return (
-      <header className={cx('siteHeader')}>
-        <nav className={cx('navbar')}>
-          <div className={cx('container')}>
-            <Head
-              toggleDropdownHandler = {this.navbarDropdownHandler}
-              promoteLink = {this.props.promoteLink}
-            />
-            <div
-              className={cx('collapse', {
-                // collapse: !this.state.navbarDropdownIsOpen,
-                in: this.state.navbarDropdownIsOpen
-              })}
-            >
-              <ul className={cx('navigationLeft')}>
-                {items.map(item =>
-                  <Item
-                    key={item.position + item.id}
-                    item={item}
-                  />
-                )}
-              </ul>
-              <ul className={cx('navigationRight')}>
-                <a href="#" className={cx('btnTransparent')}>Talk to sales</a>
-                <button className={cx('btnSuccess')}>Log in</button>
-              </ul>
+      <div>
+        <header className={cx('siteHeader')}>
+          <nav>
+            <div className="container">
+              <Head
+                toggleDropdownHandler = {this.navbarDropdownHandler}
+                promoteLink = {this.props.promoteLink}
+              />
+              <div
+                className={cx('collapse', {
+                  // collapse: !this.state.navbarDropdownIsOpen,
+                  in: this.state.navbarDropdownIsOpen
+                })}
+              >
+                <ul className={cx('navigationLeft')}>
+                  {items.map(item =>
+                    <Item
+                      key={item.position + item.id}
+                      item={item}
+                    />
+                  )}
+                </ul>
+                <ul className={cx('navigationRight')}>
+                  <a href="#" className="btn btn-transparent btn-sm"> Talk to sales </a>
+                  <button className="btn btn-success btn-sm">Log in</button>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </header>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-6">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore tempora sint quod eum labore dolorem rem nam ducimus officiis, quia?</p>
+            </div>
+            <div className="col-xs-6">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus doloremque similique, laboriosam corporis nobis vero aperiam officia voluptates, corrupti nam reprehenderit accusantium cum? Laudantium sit, nesciunt iusto sed! Dolorem eius eligendi dolorum saepe quasi nisi earum, vitae reiciendis fuga fugit! Quas eos placeat nam aspernatur doloremque eaque dolorem, consequatur officia.</p>
             </div>
           </div>
-        </nav>
-      </header>
+        </div>
+      </div>
     );
   }
 }
