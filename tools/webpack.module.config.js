841a6f0f997e2647d23692a53a4897029edefdf0
Ramiro Silveyra d'Avila <ramasilveyra1@gmail.com>
2016-07-08 16:43:31 -0300

    Don't bundle React on production bundle

--- tools/webpack.module.config.js
+++ tools/webpack.module.config.js
@@ -6,6 +6,15 @@ const config = merge({}, commonConfig, {
     filename: 'index.js',
     library: 'Auth0WebHeader',
     libraryTarget: 'umd'
+  },
+
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    }
   }
 });
 
