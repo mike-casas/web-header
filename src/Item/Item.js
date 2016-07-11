8d90c05718a6266d36d14e300a9191afce38e18f
Ramiro Silveyra d'Avila <ramasilveyra1@gmail.com>
2016-07-10 23:46:58 -0300

    Don't render tag a when there isn't a href

--- src/Item/Item.js
+++ src/Item/Item.js
@@ -5,43 +5,30 @@ import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
-const Item = ({ item, theme, simpleList, closeHeaderDropdown }) =>
-  <li
-    className={cx('item', [`item-${item.id}`], [`theme-${theme}`], {
-      positionRelative: simpleList
-    })}
-  >
-    <a
-      href={item.href}
-      onClick={closeHeaderDropdown}
-      className={cx({
-        link: !item.childrens,
-        dropdownLink: item.childrens
+const Item = ({ item, theme, simpleList, closeHeaderDropdown }) => {
+  const linkClass = cx({
+    link: !item.childrens,
+    dropdownLink: item.childrens
+  });
+  const linkContent = <ItemContent name={item.name} childrens={item.childrens} theme={theme} />;
+
+  return (
+    <li
+      className={cx('item', [`item-${item.id}`], [`theme-${theme}`], {
+        positionRelative: simpleList
       })}
     >
-      <span className={cx('text')}>{item.name}</span>
-      <span className={cx('dropdownCaret')}></span>
-      {item.childrens ?
-        <svg
-          width="10"
-          height="6"
-          viewBox="62 7 10 6"
-          className={cx('caret')}
-        >
-          <path
-            // eslint-disable-next-line max-len
-            d="M71.884 7.698l-4.56 5.116c-.013.022-.008.05-.026.07-.083.084-.192.12-.3.116-.106.004-.214-.033-.295-.117-.02-.02-.014-.047-.028-.068L62.115 7.7c-.154-.16-.154-.42 0-.58.156-.16.408-.16.563 0L67 11.97l4.322-4.85c.155-.16.406-.16.56 0 .157.16.157.418.002.578z"
-            fill={theme === 'dark' ? '#fff' : '#000'}
-          />
-        </svg>
+      {item.href
+        ? <a href={item.href} onClick={closeHeaderDropdown} className={linkClass}>{linkContent}</a>
+        : <span tabIndex="0" className={linkClass}>{linkContent}</span>
+      }
+      {item.childrens
+        ? <Dropdown data={item} closeHeaderDropdown={closeHeaderDropdown} />
         : null
       }
-    </a>
-    {item.childrens
-      ? <Dropdown data={item} closeHeaderDropdown={closeHeaderDropdown} />
-      : null
-    }
-  </li>;
+    </li>
+  );
+};
 
 Item.propTypes = {
   className: PropTypes.string,
@@ -52,3 +39,30 @@ Item.propTypes = {
 };
 
 export default Item;
+
+const ItemContent = ({ name, childrens, theme }) =>
+  <span>
+    <span className={cx('text')}>{name}</span>
+    <span className={cx('dropdownCaret')}></span>
+    {childrens ?
+      <svg
+        width="10"
+        height="6"
+        viewBox="62 7 10 6"
+        className={cx('caret')}
+      >
+        <path
+          // eslint-disable-next-line max-len
+          d="M71.884 7.698l-4.56 5.116c-.013.022-.008.05-.026.07-.083.084-.192.12-.3.116-.106.004-.214-.033-.295-.117-.02-.02-.014-.047-.028-.068L62.115 7.7c-.154-.16-.154-.42 0-.58.156-.16.408-.16.563 0L67 11.97l4.322-4.85c.155-.16.406-.16.56 0 .157.16.157.418.002.578z"
+          fill={theme === 'dark' ? '#fff' : '#000'}
+        />
+      </svg>
+      : null
+    }
+  </span>;
+
+ItemContent.propTypes = {
+  name: PropTypes.string,
+  childrens: PropTypes.array,
+  theme: PropTypes.string
+};
