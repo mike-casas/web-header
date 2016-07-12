50a314e00c47e767b902b37a9edfeee0b8fae000
Ramiro Silveyra d'Avila <ramasilveyra1@gmail.com>
2016-07-12 18:33:28 -0300

    Preload images

--- src/DropdownListItem/DropdownListItem.js
+++ src/DropdownListItem/DropdownListItem.js
@@ -1,48 +1,52 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import styles from './DropdownListItem.styl';
 import classNames from 'classnames/bind';
 
 const cx = classNames.bind(styles);
 
-const DropdownListItem = ({
-  hasArrow,
-  parentClass,
-  itemHoverHandler,
-  item,
-  closeHeaderDropdown
-}) => {
-  const logos = {
-    auth0: 'https://styleguide.auth0.com/lib/logos/img/badge.png',
-    webtask: 'https://webtask.io/images/symbol.svg'
-  };
+class DropdownListItem extends Component {
+  static propTypes = {
+    item: PropTypes.object,
+    itemHoverHandler: PropTypes.func,
+    dropdownReference: PropTypes.object,
+    hasArrow: PropTypes.bool,
+    parentClass: PropTypes.string,
+    closeHeaderDropdown: PropTypes.func
+  }
 
-  return (
-    <li
-      className={cx({
-        item: !hasArrow,
-        arrowItem: hasArrow,
-        moreItem: parentClass === 'moreDropdown'
-      })}
-      onMouseEnter={() => {itemHoverHandler(item.highlight);}}
-    >
-      <a href={item.href} onClick={closeHeaderDropdown}>
-        {item.icon
-          ? <img src={logos[item.icon]} className={cx('icon')} role="presentation" alt="" />
-          : null
-        }
-        <span className={cx('text')}>{item.name}</span>
-      </a>
-    </li>
-  );
-};
+  componentDidMount() {
+    const { item } = this.props;
+    if (item.highlight && item.highlight.thumbnail) {
+      const preloadImage = new Image();
+      preloadImage.src = item.highlight.thumbnail;
+    }
+  }
 
-DropdownListItem.propTypes = {
-  item: PropTypes.object,
-  itemHoverHandler: PropTypes.func,
-  dropdownReference: PropTypes.object,
-  hasArrow: PropTypes.bool,
-  parentClass: PropTypes.string,
-  closeHeaderDropdown: PropTypes.func
-};
+  render() {
+    const { hasArrow, parentClass, itemHoverHandler, item, closeHeaderDropdown } = this.props;
+    const logos = {
+      auth0: 'https://styleguide.auth0.com/lib/logos/img/badge.png',
+      webtask: 'https://webtask.io/images/symbol.svg'
+    };
+    return (
+      <li
+        className={cx({
+          item: !hasArrow,
+          arrowItem: hasArrow,
+          moreItem: parentClass === 'moreDropdown'
+        })}
+        onMouseEnter={() => {itemHoverHandler(item.highlight);}}
+      >
+        <a href={item.href} onClick={closeHeaderDropdown}>
+          {item.icon
+            ? <img src={logos[item.icon]} className={cx('icon')} role="presentation" alt="" />
+            : null
+          }
+          <span className={cx('text')}>{item.name}</span>
+        </a>
+      </li>
+    );
+  }
+}
 
 export default DropdownListItem;
