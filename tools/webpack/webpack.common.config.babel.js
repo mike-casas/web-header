561dd3040e776fa31b751fd706b6ac954aa30238
Ramiro Silveyra d'Avila <ramasilveyra1@gmail.com>
2017-04-11 21:28:17 -0300

    Update webpack config and deps

--- tools/webpack/webpack.common.config.babel.js
+++ tools/webpack/webpack.common.config.babel.js
@@ -10,9 +10,7 @@ if (!process.env.NODE_ENV) {
 const DEBUG = process.env.NODE_ENV === 'development';
 
 const config = {
-  entry: [
-    './src'
-  ],
+  entry: ['./src'],
 
   output: {
     path: path.join(__dirname, '../../build'),
@@ -20,59 +18,56 @@ const config = {
     filename: 'web-header.js',
     library: 'Auth0WebHeader',
     libraryTarget: 'umd'
-
   },
 
   module: {
-    rules: [{
-      test: /\.js$/,
-      use: 'babel-loader',
-      include: [path.join(__dirname, '../../src')]
-    }, {
-      test: /\.json$/,
-      use: 'json-loader'
-    }, {
-      test: /\.styl$/,
-      loader: ExtractTextPlugin.extract({
-        fallbackLoader: 'style-loader',
-        loader: [{
-          loader: 'css-loader',
-          query: {
-            sourceMap: DEBUG,
-            minimize: !DEBUG,
-            modules: true,
-            localIdentName: DEBUG ? '[path][name]--[local]--[hash:base64:5]' : '[hash:base64:4]'
-          }
-        }, {
-          loader: 'stylus-loader',
-          query: {
-            'include css': true,
-            paths: [path.resolve(__dirname, '../../node_modules')]
-          }
-        }]
-      })
-    }, {
-      test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
-      use: 'url-loader',
-      query: {
-        name: DEBUG ? '[path][name].[ext]?[hash]' : '[hash].[ext]',
-        limit: 10000
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        include: [path.join(__dirname, '../../src')]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
+        loader: 'url-loader',
+        options: {
+          name: DEBUG ? '[path][name].[ext]?[hash]' : '[hash].[ext]',
+          limit: 10000
+        }
+      },
+      {
+        test: /\.styl$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: DEBUG,
+                minimize: !DEBUG,
+                modules: true,
+                localIdentName: DEBUG ? '[path][name]--[local]--[hash:base64:5]' : '[hash:base64:4]'
+              }
+            },
+            {
+              loader: 'stylus-loader',
+              options: {
+                'include css': true,
+                paths: [path.resolve(__dirname, '../../node_modules')],
+                use: [poststylus(['autoprefixer'])],
+                sourceMap: DEBUG
+              }
+            }
+          ]
+        })
       }
-    }]
+    ]
   },
 
   plugins: [
     new webpack.LoaderOptionsPlugin({
       debug: DEBUG,
-      minimize: true,
-      options: {
-        context: __dirname,
-        stylus: {
-          use: [
-            poststylus(['autoprefixer'])
-          ]
-        }
-      }
+      minimize: !DEBUG
     }),
     // Name of the CSS bundle
     // https://github.com/webpack/extract-text-webpack-plugin/
@@ -86,28 +81,27 @@ const config = {
       'process.env.NODE_ENV': DEBUG ? '"development"' : '"production"'
     }),
 
-    ...DEBUG ? [] : [
-      // Minimize all JavaScript output of chunks
-      // https://github.com/mishoo/UglifyJS2#compressor-options
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          screw_ie8: true, // React doesn't support IE8
-          warnings: false
-        },
-        mangle: {
-          screw_ie8: true
-        },
-        output: {
-          comments: false,
-          screw_ie8: true
-        }
-      }),
-
-      // A plugin for a more aggressive chunk merging strategy
-      // https://webpack.github.io/docs/list-of-plugins.html#aggressivemergingplugin
-      new webpack.optimize.AggressiveMergingPlugin()
-    ],
-    new webpack.NoErrorsPlugin()
+    ...(DEBUG
+      ? []
+      : [
+        // Minimize all JavaScript output of chunks
+        // https://github.com/mishoo/UglifyJS2#compressor-options
+        new webpack.optimize.UglifyJsPlugin({
+          compress: {
+            screw_ie8: true, // React doesn't support IE8
+            warnings: false
+          },
+          mangle: {
+            screw_ie8: true
+          },
+          output: {
+            comments: false,
+            screw_ie8: true
+          }
+        })
+      ]
+    ),
+    new webpack.NoEmitOnErrorsPlugin()
   ],
 
   cache: DEBUG,
